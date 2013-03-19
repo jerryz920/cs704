e944267d72f93ed6856da47d93fbea60d2fdff83
Keqiang He <keqhe@swallow.cs.wisc.edu>
2013-03-19 17:34:33 -0500

    freevars and applyToTypeExp; please helpcheck

---
+++ Substituion.ml
@@ -0,0 +1,28 @@
+open Lambda ;;
+
+type subs = (string * typ) list (*subs is a list of pair (type-variable,unquantified-type-expressions), that is, a list of (variable,'typ')*)
+
+let rec freevars (t:gentyp) : string list = 
+	let non_frees = fst(t) in
+	let typ_e = snd(t) in 
+	match typ_e with
+	Int 	->[]
+	|Bool  -> []
+	|Sym  -> []
+	|Tvar(tl) -> if List.mem tl non_frees then [] else [tl]
+	|List(tl) -> (freevars (non_frees, tl))
+	|Fun(tl,tr) ->List.append (freevars (non_frees, tl)) (freevars (non_frees, tr))
+	;;
+(*testcase freevars ([],Fun(Tvar("t1"),Fun(Tvar("t2"),Int)));;*)	
+let rec applyToTypeExp (s:subs) (gentyp_exp:gentyp) : typ =
+	let frees = freevars gentyp_exp in
+	let typ_e = snd(gentyp_exp) in
+	match typ_e with
+	|Int -> Int
+	|Bool -> Bool
+	|Sym -> Sym
+	|Tvar(tl) ->if (List.mem tl frees && List.mem_assoc tl s) then List.assoc tl s else typ_e(*if tl is free variable in gentyp_exp && there is a binding in s for tl*)
+	|List(tl) -> List (applyToTypeExp s (fst(gentyp_exp),tl))
+	|Fun(tl,tr) -> Fun((applyToTypeExp s (fst(gentyp_exp),tl)),(applyToTypeExp s (fst(gentyp_exp),tr)))
+	;;
+ 
