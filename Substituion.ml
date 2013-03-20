cfde920dc982a3367cdc17eae5eaca87ea0d6514
Keqiang He <keqhe@swallow.cs.wisc.edu>
2013-03-20 02:03:56 -0500

    added new test cases for Part2-a,b; finished part2-c, that is, compose function and tested it

--- Substituion.ml
+++ Substituion.ml
@@ -14,7 +14,8 @@ let rec freevars (t:gentyp) : string list =
 	|List(tl) -> (freevars (non_frees, tl))
 	|Fun(tl,tr) ->List.append (freevars (non_frees, tl)) (freevars (non_frees, tr))
 	;;
-(*testcase:freevars ([],Fun(Tvar("t1"),Fun(Tvar("t2"),Int)));;*)	
+(*testcase:freevars ([],Fun(Tvar("t1"),Fun(Tvar("t2"),Int)));;
+freevars (["t1"],Fun(Tvar("t1"),Fun(Tvar("t2"),Int)));;*)	
 let rec applyToTypeExp (s:subs) (gentyp_exp:gentyp) : typ =
 	let frees = freevars gentyp_exp in
 	let typ_e = snd(gentyp_exp) in
@@ -26,7 +27,8 @@ let rec applyToTypeExp (s:subs) (gentyp_exp:gentyp) : typ =
 	|List(tl) -> List (applyToTypeExp s (fst(gentyp_exp),tl))
 	|Fun(tl,tr) -> Fun((applyToTypeExp s (fst(gentyp_exp),tl)),(applyToTypeExp s (fst(gentyp_exp),tr)))
 	;;
-(*testcase: applyToTypeExp [("t1",Int)] ([],Fun(Tvar("t1"),Fun(Tvar("t2"),Int))) *) 
+(*testcase: applyToTypeExp [("t1",Int)] ([],Fun(Tvar("t1"),Fun(Tvar("t2"),Int))) 
+applyToTypeExp [("t1",Int)] (["t1"],Fun(Tvar("t1"),Fun(Tvar("t2"),Int)))*) 
 let rec applyToTypeEnv (s:subs) (env_list:env) : env = 
 	match env_list with
 	|[] -> []
@@ -35,3 +37,24 @@ let rec applyToTypeEnv (s:subs) (env_list:env) : env =
 	;;
 (*testcase: applyToTypeEnv [("t1",Int)] init_typenv;;
 applyToTypeEnv [("t1",Int)] (add_entry ("f",([],Tvar("t1"))) init_typenv)  *)
+let compose (s1:subs) (s2:subs) : subs =
+	let t = s2 in
+	let rec apply_s1_to_t (x:subs) : subs=
+	match x with 
+	|[] -> []
+	|e::left -> let variable = fst(e) in let typ_exp =snd(e) in 
+			let part = (variable,(applyToTypeExp s1 ([],typ_exp))) 
+			in part::(apply_s1_to_t left)
+	in
+	let applied_t = apply_s1_to_t t
+	in
+	let rec add_to_t (y:subs) (u:subs) :subs=
+	match y with
+	|[] -> []
+	|e1::left1 -> let typ_v1 = fst(e1) in 
+	(*	if (List.mem_assoc typ_v1 applied_t) then (List.append applied_t (add_to_t left1)) else (List.append (e1::applied_t) (add_to_t left1))   *)
+	if (List.mem_assoc typ_v1 u) then (List.append u (add_to_t left1 u)) else 
+		let newu = e1::u in (List.append newu (add_to_t left1 newu))
+	in
+	add_to_t s1 applied_t
+	;;
